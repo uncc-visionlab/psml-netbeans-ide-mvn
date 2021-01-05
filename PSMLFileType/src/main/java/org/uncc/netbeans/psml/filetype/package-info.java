613fa8329091d994c67b888fcb2aeb166c42afe9
Andrew Willis <arwillis@uncc.edu>
2021-01-04 19:40:58 -0500

    Changed the coordinates for source files and resources. Updated POM files for new coordinates.

--- PSMLFileType/src/main/java/org/uncc/netbeans/psml/filetype/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-@TemplateRegistration(folder = "Other", content = "PSMLTemplate.psml")
-package org.uncc.netbeans.psml.filetype;
-
-import org.netbeans.api.templates.TemplateRegistration;
